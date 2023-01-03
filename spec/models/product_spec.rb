83ba82bd5aaf6fca4b131dee6250d506f99a9f1e
Ben Stolb <benstolb@protonmail.com>
2023-01-03 15:14:56 -0800

    Add product tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it 'is valid with valid attributes' do
+      product = Product.new(name: "Lily of the Valley", price: 100, quantity: 5, category: Category.new)
+      expect(product).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      product = Product.new(name: nil, price: 100, quantity: 5, category: Category.new)
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'is not valid without a price' do
+      product = Product.new(name: "Lily of the Valley", price: false, quantity: 5, category: Category.new)
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Price is not a number") 
+    end
+
+    it 'is not valid without a quantity' do
+      product = Product.new(name: "Lily of the Valley", price: 100, quantity: nil, category: Category.new)
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'is not valid without a category' do
+      product = Product.new(name: "Lily of the Valley", price: 100, quantity: 5, category: nil)
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include("Category must exist")
+    end
+  end
 end
