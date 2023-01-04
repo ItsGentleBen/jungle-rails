9f7cba1adf34ea87a441a2f0f24fa9ba182576fb
Ben Stolb <benstolb@protonmail.com>
2023-01-03 16:08:12 -0800

    refactor product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,35 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  describe 'Validations' do
-
-    it 'is valid with valid attributes' do
-      product = Product.new(name: "Lily of the Valley", price: 100, quantity: 5, category: Category.new)
-      expect(product).to be_valid
+  describe "Validations" do
+    before(:each) do
+      @category = Category.new
+      @product = Product.new( 
+        name: "Lily of the Valley",
+        price: 100,
+        quantity: 50,
+        category: @category
+      )
     end
 
+    it 'is valid with valid attributes' do
+      expect(@product.validate).to be true
+      expect(@product.valid?).to be true
+      expect(@product).to be_valid
+      expect(@product.errors.full_messages.any?).to be false
+    end
+  
     it 'is not valid without a name' do
-      product = Product.new(name: nil, price: 100, quantity: 5, category: Category.new)
-      expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include("Name can't be blank")
+      @product.name = ''
+      expect(@product.validate).to be false
+      expect(@product.valid?).to be false
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include ("Name can't be blank")
+      expect(@product.errors.full_messages[0]).to include ("can't be blank")
+      expect(@product.errors.full_messages.any?).to be true
     end
 
     it 'is not valid without a price' do
-      product = Product.new(name: "Lily of the Valley", price: false, quantity: 5, category: Category.new)
-      expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include("Price is not a number") 
+      @product.price = :blank
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include ("Price is not a number")
     end
-
-    it 'is not valid without a quantity' do
-      product = Product.new(name: "Lily of the Valley", price: 100, quantity: nil, category: Category.new)
-      expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include("Quantity can't be blank")
+   
+    it 'is not valid when quantity is blank' do
+      @product.quantity = :blank
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include ("Quantity can't be blank")
     end
 
     it 'is not valid without a category' do
-      product = Product.new(name: "Lily of the Valley", price: 100, quantity: 5, category: nil)
-      expect(product).to_not be_valid
-      expect(product.errors.full_messages).to include("Category must exist")
+      @product.category = nil
+      expect(@product.valid?).to be false
+      expect(@product.errors.full_messages).to include ("Category must exist")
     end
   end
 end
